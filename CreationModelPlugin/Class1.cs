ad163a31e5fa7afc61473e4b44f9169021ecb060
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2022-03-27 21:40:06 +0300

    Задание5

--- CreationModelPlugin/Class1.cs
+++ CreationModelPlugin/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
@@ -12,14 +13,18 @@ namespace CreationModelPlugin
     [TransactionAttribute(TransactionMode.Manual)]
     public class CreationModel : IExternalCommand
     {
+
+
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            
             Document doc = commandData.Application.ActiveUIDocument.Document;
 
             WallCreate(doc);
 
             return Result.Succeeded;
         }
+
 
         private void WallCreate(Document doc)
         {
@@ -56,8 +61,75 @@ namespace CreationModelPlugin
                 Wall wall = Wall.Create(doc, line, level1.Id, false);
                 walls.Add(wall);
                 wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
+
             }
+            AddDoor(doc, level1, walls[0]);
+            AddWindows(doc, level1, walls[1], walls[2], walls[3]);
             transaction.Commit();
+        }
+
+        private void AddDoor(Document doc, Level level1, Wall wall)
+        {
+            FamilySymbol doorType = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_Doors)
+                .OfType<FamilySymbol>()
+                .Where(x => x.Name.Equals("0915 x 2134 мм"))
+                .Where(x => x.FamilyName.Equals("Одиночные-Щитовые"))
+                .FirstOrDefault();
+
+
+            LocationCurve hostCurve = wall.Location as LocationCurve;
+            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
+            XYZ point2 = hostCurve.Curve.GetEndPoint(1);
+            XYZ point = (point1 + point2) / 2;
+
+            if (!doorType.IsActive)
+                doorType.Activate();
+
+            doc.Create.NewFamilyInstance(point, doorType, wall, level1, StructuralType.NonStructural);
+        }
+
+        private void AddWindows(Document doc, Level level1, Wall wall1, Wall wall2, Wall wall3)
+        {
+            FamilySymbol windowType = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_Windows)
+                .OfType<FamilySymbol>()
+                .Where(x => x.Name.Equals("0915 x 1220 мм"))
+                .Where(x => x.FamilyName.Equals("Фиксированные"))
+                .FirstOrDefault();
+
+
+            LocationCurve hostCurve1 = wall1.Location as LocationCurve;
+            XYZ point1 = hostCurve1.Curve.GetEndPoint(0);
+            XYZ point2 = hostCurve1.Curve.GetEndPoint(1);
+            XYZ point = (point1 + point2) / 2;
+
+            if (!windowType.IsActive)
+                windowType.Activate();
+
+            doc.Create.NewFamilyInstance(point, windowType, wall1, level1, StructuralType.NonStructural);
+
+            LocationCurve hostCurve2 = wall2.Location as LocationCurve;
+            XYZ point3 = hostCurve2.Curve.GetEndPoint(0);
+            XYZ point4 = hostCurve2.Curve.GetEndPoint(1);
+            XYZ point7 = (point3 + point4) / 2;
+
+            if (!windowType.IsActive)
+                windowType.Activate();
+
+            doc.Create.NewFamilyInstance(point7, windowType, wall2, level1, StructuralType.NonStructural);
+
+            LocationCurve hostCurve3 = wall3.Location as LocationCurve;
+            XYZ point5 = hostCurve3.Curve.GetEndPoint(0);
+            XYZ point6 = hostCurve3.Curve.GetEndPoint(1);
+            XYZ point8 = (point5 + point6) / 2;
+
+            if (!windowType.IsActive)
+                windowType.Activate();
+
+            doc.Create.NewFamilyInstance(point8, windowType, wall3, level1, StructuralType.NonStructural);
         }
     }
 }
